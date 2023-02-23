f9b0fec45499401655a87b9e064abb9039e00aa9
csae8092 <peter.andorfer@oeaw.ac.at>
2023-02-23 10:38:29 +0100

    updates

--- scripts/make_rdf.py
+++ scripts/make_rdf.py
@@ -1,8 +1,8 @@
 import os
 from tqdm import tqdm
 from acdh_cidoc_pyutils import (
-    make_appelations,
-    make_ed42_identifiers,
+    make_appellations,
+    make_e42_identifiers,
     coordinates_to_p168,
     make_birth_death_entities,
     make_occupations,
@@ -35,8 +35,8 @@ for x in tqdm(items, total=len(items)):
     name_node = x.xpath(".//tei:persName", namespaces=nsmap)[0]
     item_label = make_entity_label(name_node)[0]
     g.add((subj, RDF.type, CIDOC["E21_Person"]))
-    g += make_ed42_identifiers(subj, x, type_domain=f"{SK}types", default_lang="und")
-    g += make_appelations(subj, x, type_domain=f"{SK}types", default_lang="und")
+    g += make_e42_identifiers(subj, x, type_domain=f"{SK}types", default_lang="und", same_as=False)
+    g += make_appellations(subj, x, type_domain=f"{SK}types", default_lang="und")
     g += make_occupations(subj, x, default_lang="de")[0]
     g += make_affiliations(
         subj,
@@ -82,8 +82,8 @@ for x in tqdm(items, total=len(items)):
     item_id = f"{SK}{xml_id}"
     subj = URIRef(item_id)
     g.add((subj, RDF.type, CIDOC["E74_Group"]))
-    g += make_appelations(subj, x, type_domain=f"{SK}types/", default_lang="und")
-    g += make_ed42_identifiers(subj, x, type_domain=f"{SK}types", default_lang="und")
+    g += make_appellations(subj, x, type_domain=f"{SK}types/", default_lang="und")
+    g += make_e42_identifiers(subj, x, type_domain=f"{SK}types", default_lang="und")
 
 # PLACES
 entity_type = "place"
@@ -100,8 +100,8 @@ for x in tqdm(items, total=len(items)):
     subj = URIRef(item_id)
     g.add((subj, RDF.type, CIDOC["E53_Place"]))
     g += coordinates_to_p168(subj, x)
-    g += make_appelations(subj, x, type_domain=f"{SK}types/", default_lang="und")
-    g += make_ed42_identifiers(subj, x, type_domain=f"{SK}types", default_lang="und")
+    g += make_appellations(subj, x, type_domain=f"{SK}types/", default_lang="und")
+    g += make_e42_identifiers(subj, x, type_domain=f"{SK}types", default_lang="und")
     try:
         pmb = x.xpath('.//tei:idno[@type="pmb"]/text()', namespaces=nsmap)[0]
     except IndexError:
