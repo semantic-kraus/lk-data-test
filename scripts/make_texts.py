3f0ed16cd7ce8302aff4727084216762f6149fbf
csae8092 <peter.andorfer@oeaw.ac.at>
2023-02-24 11:29:33 +0100

    lk documents and cases

--- scripts/make_texts.py
+++ scripts/make_texts.py
@@ -13,8 +13,10 @@ rdf_dir = "./rdf"
 os.makedirs(rdf_dir, exist_ok=True)
 domain = "https://sk.acdh.oeaw.ac.at/"
 SK = Namespace(domain)
+title_type = URIRef(f"{SK}types/title/prov")
+
 g = Graph()
-LIMIT = False
+LIMIT = 100
 entity_type = "documents"
 if LIMIT:
     files = sorted(glob.glob("legalkraus-archiv/data/editions/*.xml"))[:LIMIT]
@@ -40,14 +42,57 @@ for x in tqdm(to_process, total=len(to_process)):
     )
     item_id = f"{SK}{xml_id}"
     subj = URIRef(item_id)
+    subj_f4 = URIRef(f"{item_id}/carrier")
     item_label = normalize_string(doc.any_xpath(".//tei:title[1]/text()")[0])
-    g.add((subj, RDF.type, FRBROO["F22"]))
+    g.add((subj, RDF.type, CIDOC["E73_Information_Object"]))
+# DOC-ARCHE-IDs
+    arche_id = URIRef(f"{SK}identifier/{xml_id}")
+    arche_id_value = f"https://id.acdh.oeaw.ac.at/legalkraus/{xml_id}.xml"
+    g.add((
+        subj, CIDOC["P1_is_identified_by"], arche_id
+    ))
+    g.add((
+        arche_id, RDF.type, CIDOC["E42_Identifier"]
+    ))
+    g.add((
+        arche_id, RDF.value, Literal(arche_id_value, datatype=XSD.anyURI)
+    ))
+    g.add((
+        arche_id, RDFS.label, Literal(f"ARCHE-ID: {arche_id_value}", lang="en")
+    ))
+# appellations
+    title_uri = URIRef(f"{subj}/title/0")
+    g.add((
+        title_uri, RDF.type, CIDOC["E35_Title"]
+    ))
+    g.add((
+        title_uri, RDF.value, Literal(item_label, lang="de")
+    ))
+    g.add((
+        title_uri, RDFS.label, Literal(item_label, lang="de")
+    ))
+    g.add((subj, CIDOC["P102_has_title"], title_uri))
+    g.add((
+        title_uri, CIDOC["P2_has_type"], title_type
+    ))
+
+
+# F4_Manifestation
+    g.add((
+        subj_f4, RDF.type, FRBROO["F4_Manifestation_Singleton"]
+    ))
+    g.add((
+        subj_f4, RDFS.label, Literal(f"Carrier of: {item_label}")
+    ))
+    g.add((
+        subj_f4, CIDOC["P128_carries"], subj
+    ))
     g.add((subj, RDFS.label, Literal(item_label, lang="de")))
     creation_uri = URIRef(f"{subj}/creation")
-    g.add((creation_uri, RDF.type, FRBROO["F28"]))
-    g.add((creation_uri, RDFS.label, Literal(f"Creation Event of {item_label}")))
-    g.add((creation_uri, FRBROO["R17"], subj))
-    g.add((subj, FRBROO["R17i"], subj))
+    g.add((creation_uri, RDF.type, CIDOC["E65_Creation"]))
+    g.add((creation_uri, RDFS.label, Literal(f"Creation of: {item_label}")))
+    # g.add((creation_uri, FRBROO["R17"], subj))
+    # g.add((subj, FRBROO["R17i"], subj))
 
     # creation date:
     try:
@@ -57,7 +102,7 @@ for x in tqdm(to_process, total=len(to_process)):
         go_on = False
     if go_on:
         begin, end = extract_begin_end(creation_date_node)
-        creation_ts = URIRef(f"{creation_uri}/timestamp")
+        creation_ts = URIRef(f"{creation_uri}/time-span")
         g += create_e52(creation_ts, begin_of_begin=begin, end_of_end=end)
 
     # creator Brief:
@@ -70,44 +115,111 @@ for x in tqdm(to_process, total=len(to_process)):
         creator_uri = URIRef(f"{SK}{creator[1:]}")
         g.add((creation_uri, CIDOC["P14_carried_out_by"], creator_uri))
 
-    # fun with mentions
-    for i, mention in enumerate(doc.any_xpath('.//tei:body//tei:rs[@ref and @type="person"]')):
-        try:
-            pb_start = mention.xpath('.//preceding::tei:pb/@n', namespaces=NSMAP)[-1]
-        except IndexError:
-            pb_start = 1
-        try:
-            pb_end = mention.xpath('.//following::tei:pb/@n', namespaces=NSMAP)[0]
-        except IndexError:
-            pb_end = pb_start
-        person_id = mention.attrib["ref"][1:]
-        person_uri = URIRef(f"{SK}{person_id}")
-        mention_string = normalize_string(" ".join(mention.xpath(".//text()")))
-        mention_event_uri = URIRef(f"{subj}/mention-event/{person_id}/{i}")
-        mention_singleton_uri = URIRef(f"{subj}/mention-event/{person_id}/{i}/f4")
-        mention_segment = URIRef(f"{mention_event_uri}/int16")
-        g.add((mention_event_uri, RDF.type, CIDOC["E5_Event"]))
-        g.add(
-            (
-                mention_event_uri,
-                RDFS.label,
-                Literal(f"Event: {item_label} erwähnt {mention_string}", lang="de"),
-            )
-        )
-        g.add((mention_event_uri, CIDOC["P11_had_participant"], person_uri))
-        g.add(
-            (
-                mention_event_uri,
-                CIDOC["P12_occurred_in_the_presence_of"],
-                mention_singleton_uri,
-            )
-        )
-        g.add((mention_singleton_uri, RDF.type, FRBROO["F4"]))
-        g.add((mention_singleton_uri, CIDOC["P128_carries"], mention_segment))
-        g.add((mention_segment, RDF.type, INT["INT16_PublicationExpressionSection"]))
-        g.add((mention_segment, INT["pageEnd"], Literal(pb_end)))
-        g.add((mention_segment, INT["pageStart"], Literal(pb_start)))
-        g.add((mention_segment, CIDOC["P165_incorporates"],subj ))
+    # # fun with mentions
+    # for i, mention in enumerate(doc.any_xpath('.//tei:body//tei:rs[@ref and @type="person"]')):
+    #     try:
+    #         pb_start = mention.xpath('.//preceding::tei:pb/@n', namespaces=NSMAP)[-1]
+    #     except IndexError:
+    #         pb_start = 1
+    #     try:
+    #         pb_end = mention.xpath('.//following::tei:pb/@n', namespaces=NSMAP)[0]
+    #     except IndexError:
+    #         pb_end = pb_start
+    #     person_id = mention.attrib["ref"][1:]
+    #     person_uri = URIRef(f"{SK}{person_id}")
+    #     mention_string = normalize_string(" ".join(mention.xpath(".//text()")))
+    #     mention_event_uri = URIRef(f"{subj}/mention-event/{person_id}/{i}")
+    #     mention_singleton_uri = URIRef(f"{subj}/mention-event/{person_id}/{i}/f4")
+    #     mention_segment = URIRef(f"{mention_event_uri}/int16")
+    #     g.add((mention_event_uri, RDF.type, CIDOC["E5_Event"]))
+    #     g.add(
+    #         (
+    #             mention_event_uri,
+    #             RDFS.label,
+    #             Literal(f"Event: {item_label} erwähnt {mention_string}", lang="de"),
+    #         )
+    #     )
+    #     g.add((mention_event_uri, CIDOC["P11_had_participant"], person_uri))
+    #     g.add(
+    #         (
+    #             mention_event_uri,
+    #             CIDOC["P12_occurred_in_the_presence_of"],
+    #             mention_singleton_uri,
+    #         )
+    #     )
+    #     g.add((mention_singleton_uri, RDF.type, FRBROO["F4"]))
+    #     g.add((mention_singleton_uri, CIDOC["P128_carries"], mention_segment))
+    #     g.add((mention_segment, RDF.type, INT["INT16_PublicationExpressionSection"]))
+    #     g.add((mention_segment, INT["pageEnd"], Literal(pb_end)))
+    #     g.add((mention_segment, INT["pageStart"], Literal(pb_start)))
+    #     g.add((mention_segment, CIDOC["P165_incorporates"],subj ))
+
+
+# cases
+print("lets process cases as E5 Events")
+
+if LIMIT:
+    files = sorted(glob.glob("legalkraus-archiv/data/cases_tei/*.xml"))[:LIMIT]
+else:
+    files = sorted(glob.glob("legalkraus-archiv/data/cases_tei/*.xml"))
+
+for x in tqdm(files, total=len(files)):
+    doc = TeiReader(x)
+    xml_id = (
+        doc.tree.getroot()
+        .attrib["{http://www.w3.org/XML/1998/namespace}id"]
+        .replace(".xml", "")
+    )
+    item_id = f"{SK}{xml_id}"
+    subj = URIRef(item_id)
+    item_label = normalize_string(doc.any_xpath(".//tei:title[1]/text()")[0])
+    item_comment = normalize_string(doc.any_xpath(".//tei:abstract[1]/tei:p//text()")[0])
+    
+    g.add((subj, RDF.type, CIDOC["E5_Event"]))
+    g.add((subj, RDFS.label, Literal(item_label, lang="de")))
+    g.add((
+        subj, RDFS.comment, Literal(item_comment, lang="de")
+    ))
+    # appellations
+    app_uri = URIRef(f"{subj}/appellation/0")
+    g.add((
+        app_uri, RDF.type, CIDOC["E33_E41_Linguistic_Appellation"]
+    ))
+    g.add((
+        app_uri, RDF.value, Literal(item_label, lang="de")
+    ))
+    g.add((
+        app_uri, RDFS.label, Literal(item_label, lang="de")
+    ))
+    g.add((subj, CIDOC["P1_is_identified_by"], app_uri))
+
+# DOC-ARCHE-IDs
+    arche_id = URIRef(f"{SK}identifier/{xml_id}")
+    arche_id_value = f"https://id.acdh.oeaw.ac.at/legalkraus/{xml_id}.xml"
+    g.add((
+        subj, CIDOC["P1_is_identified_by"], arche_id
+    ))
+    g.add((
+        arche_id, RDF.type, CIDOC["E42_Identifier"]
+    ))
+    g.add((
+        arche_id, RDF.value, Literal(arche_id_value, datatype=XSD.anyURI)
+    ))
+    g.add((
+        arche_id, RDFS.label, Literal(f"ARCHE-ID: {arche_id_value}", lang="en")
+    ))
+
+    # linked documents
+    for y in doc.any_xpath('.//tei:list[@type="objects"]//tei:ref/text()'):
+        doc_xml_id = y.replace('.xml', '')
+        doc_uri = URIRef(f"{SK}{doc_xml_id}")
+        g.add((
+            doc_uri, CIDOC["P12_occurred_in_the_presence_of"], subj
+        ))
+
+
+
+
 
 print("writing graph to file")
 g.serialize(f"{rdf_dir}/texts.ttl")
