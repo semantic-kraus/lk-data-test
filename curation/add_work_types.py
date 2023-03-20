f0789714271ea0fc7c57baaa0671e1f386aff8cc
csae8092 <peter.andorfer@oeaw.ac.at>
2023-03-20 10:24:14 +0100

    trigger build wf manually; add default type

--- curation/add_work_types.py
+++ curation/add_work_types.py
@@ -9,12 +9,15 @@ doc = TeiReader(list_work)
 
 all = 0
 bibls = doc.any_xpath('.//tei:listBibl/tei:bibl')
+print("deleting existing tpyes")
+for x in doc.any_xpath('.//tei:bibl[@type="sk"]'):
+    x.attrib["subtype"] = "no_type"
 for x in work_types:
     items = doc.any_xpath(x["type_xpath"])
-    print(len(items))
+    print(f"typed {len(items)} bibls with type: {x['type_value']}")
     for sk_work in items:
         sk_work.attrib["subtype"] = x["type_value"]
     all += len(items)
 print(f"caterogized: {all} items out of {len(bibls)}")
 
-doc.tree_to_file("list_work")
+doc.tree_to_file("hansi.xml")
