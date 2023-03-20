83c45221d9b3ad3ab89dab8063a9397db66d6885
csae8092 <peter.andorfer@oeaw.ac.at>
2023-03-20 08:29:58 +0100

    added script to add better keys

---
+++ curation/fix_journal_keys.py
@@ -0,0 +1,27 @@
+from acdh_tei_pyutils.tei import TeiReader
+
+doc = TeiReader("./data/indices/listwork.xml")
+items = doc.any_xpath(f".//tei:listBibl/tei:bibl[./tei:bibl/tei:title[@level='j']]")
+nsmap = doc.nsmap
+print(len(nsmap))
+
+pmb_id_dict = {}
+for x in items:
+    xml_id = x.attrib["{http://www.w3.org/XML/1998/namespace}id"]
+    sk_node = x.xpath('./tei:bibl[@type="sk"]', namespaces=nsmap)[0]
+    child_nodes = sk_node.xpath('.//*')
+    if len(child_nodes) == 1:
+        print(child_nodes[0].text)
+        key = child_nodes[0].attrib['key']
+        pmb_id_dict[key] = xml_id
+        # child_nodes[0].attrib["newkey"] = f"#{xml_id}"
+
+
+for x in doc.any_xpath(".//*[@key]"):
+    try:
+        new_value = pmb_id_dict[x.attrib["key"]]
+    except KeyError:
+        continue
+    x.attrib["key"] = f"#{new_value}"
+
+doc.tree_to_file('hansi.xml')
