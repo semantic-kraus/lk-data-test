170dbc5eeaa8311c8d2253f24ba0338940b530c2
BOberreither <37076522+BOberreither@users.noreply.github.com>
2023-03-20 10:12:23 +0100

    Update work_type_mapping.py
    added / differentiated standalones with and without publication expression

--- curation/work_type_mapping.py
+++ curation/work_type_mapping.py
@@ -12,7 +12,11 @@ work_types = [
         "type_xpath": '//tei:bibl[@type="sk"][./tei:title[@level="j"] and ./tei:title[@level="a"] and ./tei:date[@key]]',
     },
     {
+        "type_value": "standalone text",
+        "type_xpath": '//tei:bibl[@type="sk"][./tei:title[@level="m"] and not(parent::tei:bibl//tei:date)]',
+    },
+    {
         "type_value": "standalone publication",
-        "type_xpath": '//tei:bibl[@type="sk"][./tei:title[@level="m"] and not(./tei:date)]',
+        "type_xpath": '//tei:bibl[@type="sk"][./tei:title[@level="m"] and parent::tei:bibl//tei:date]',
     },
 ]
