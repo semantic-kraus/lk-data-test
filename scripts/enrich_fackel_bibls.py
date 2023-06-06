92650b1c106cc40d88b26df28a6e8a1471a2dbb8
cfhaak <c.f.haak@posteo.de>
2023-06-06 11:51:31 +0200

    script for #66 ; updated requirements

--- scripts/enrich_fackel_bibls.py
+++ scripts/enrich_fackel_bibls.py
@@ -3,17 +3,20 @@ from acdh_tei_pyutils.tei import TeiReader
 from lxml import etree as ET
 import lxml.builder
 
-listfackel_path = "./legalkraus-archiv/data/indices/listfackel.xml"
+listfackel_path = "./data/indices/listfackel.xml"
 listfackel = TeiReader(listfackel_path)
-fackel_texts = TeiReader("../fa-data/data/indices/fackelTexts_cascaded.xml")
+fackel_texts = TeiReader("https://raw.githubusercontent.com/semantic-kraus/fa-data/main/data/indices/fackelTexts_cascaded.xml")
 nsmap = fackel_texts.nsmap
-teiMaker = lxml.builder.ElementMaker(
-    namespace="http://www.tei-c.org/ns/1.0", nsmap=nsmap
-)
+teiMaker = lxml.builder.ElementMaker()
 single_machtes = 0
-multimatches = 0
 nomatches = 0
-nolinkmatches = 0
+unmatched = []
+
+def delete_old_matches():
+    # # need to delete old matches, befor I create new ones
+    # # be careful to only delete exact matches
+    for idno in listfackel.any_xpath("//tei:idno[@type='fackel' and (@subtype='text' or subtype='issue')]"):
+        idno.getparent().remove(idno)
 
 
 class BiblIf:
@@ -58,93 +61,79 @@ for text in fackel_texts.any_xpath("//issue"):
 def search_4_listfackel_bibl(listfackel_bibl: BiblIf):
     global nomatches
     global single_machtes
-    global multimatches
-    global nolinkmatches
     matches = []
+    # # @corresp with //@webLink
     for key, val in weblinks_2_fackeltexts.items():
         if listfackel_bibl.corresp == key:
             matches += val
-    if not matches:
-        print(listfackel_bibl.corresp)
-        nolinkmatches += 1
-    linkmatches = matches if len(matches) == 2 else []
-    if len(matches) > 1:
-        if listfackel_bibl.bibl_scope_from:
-            matches = [
-                match
-                for match in matches
-                if match.get("startPage") == listfackel_bibl.bibl_scope_from
-            ]
-    if len(matches) > 1:
-        if listfackel_bibl.bibl_scope_to:
-            matches = [
-                match
-                for match in matches
-                if match.get("endPage") == listfackel_bibl.bibl_scope_to
-            ]
+    # # and biblScope:
+    # # biblScope/@from with text/@startPage
+    matches = [
+        match
+        for match in matches
+        if match.get("startPage") == listfackel_bibl.bibl_scope_from
+    ]
+    # # biblScope/@to with text/@endPage
+    matches = [
+        match
+        for match in matches
+        if match.get("endPage") == listfackel_bibl.bibl_scope_to
+    ]
+    # # If there is more than one corresponding text-element in fackelTexts_cascaded.xml, 
+    # # compare listfackel.xml's title[@level="a"] with text/@titleText there. 
+    # # Take the one that fits better.
     if len(matches) > 1 and listfackel_bibl.title:
         matches = [
             match
             for match in matches
             if match.get("titleText").lower().strip(" .") == listfackel_bibl.title
         ]
-    if len(matches) == 0 and linkmatches:
-        if not [
-            match
-            for match in matches
-            if match.get("titleText").lower().strip(" .") == listfackel_bibl.title
-        ]:
-            match1 = len(linkmatches[0].xpath("ancestor::tei:text", namespaces=nsmap))
-            match2 = len(linkmatches[1].xpath("ancestor::tei:text", namespaces=nsmap))
-            if match1 > match2:
-                matches = linkmatches[:1]
-            else:
-                matches = linkmatches[1:]
-    if len(matches) == 1:
+    
+    # # if there is no corresponding text-element in fackelTexts_cascaded.xml, 
+    # # look if the @corresp-value from listfackel can be found in a @range in 
+    # # fackelTexts_cascaded.xml. If so, take that text's ID.
+    if len(matches) == 0:
+        matches = fackel_texts.any_xpath(f"//text[contains(translate(@range, 'F', 'f'), '{listfackel_bibl.corresp}')]")
+        print(f"matches was 0 for {listfackel_bibl.corresp} but {len(matches)} by range.")
+    matched = matches[0] if len(matches) >= 1 else None
+    if matched is not None:
+        matched_id = matched.get("id")
+        listfackel_bibl.element.insert(0, teiMaker.idno(matched_id, type="fackel", subtype="text"))
         single_machtes += 1
-        matched_id = matches[0].get("id")
-        listfackel_bibl.element.insert(0, teiMaker.idno(matched_id, type="fackel"))
-    elif len(matches) > 1:
-        multimatches += 1
-        print(f"{listfackel_bibl.corresp} unclear match, {len(matches)} matches")
     else:
-        print(listfackel_bibl.corresp)
-        print(f"{len(matches)} matches\n")
         nomatches += 1
+        print(listfackel_bibl.corresp)
 
 
 def search_4_listfackel_issue_bibl(listfackel_bibl: BiblIf):
     global nomatches
     global single_machtes
-    global multimatches
-    global nolinkmatches
     matches = []
     for key, val in weblinks_2_fackelissues.items():
         if listfackel_bibl.corresp == key:
             matches += val
-    if not matches:
-        print(listfackel_bibl.corresp)
-        nolinkmatches += 1
     if len(matches) == 1:
+        matched_id = matches[0].get("id")
+        listfackel_bibl.element.insert(0, teiMaker.idno(matched_id, type="fackel", subtype="issue"))
         single_machtes += 1
-    elif len(matches) > 1:
-        print(f"{listfackel_bibl.corresp} unclear match, {len(matches)} matches")
-        multimatches += 1
     else:
-        print(listfackel_bibl.corresp)
-        print(f"{len(matches)} matches\n")
+        print(f"bibl with corresp '{listfackel_bibl.corresp}' couldn't be matched at all.")
         nomatches += 1
 
 
 if __name__ == "__main__":
     listfackel_bibls = []
+    delete_old_matches()
     for bibl in listfackel.any_xpath("//tei:body/tei:listBibl/tei:bibl"):
         listfackel_bibls.append(BiblIf(bibl))
     for listfackel_bibl in listfackel_bibls:
         if not listfackel_bibl.issue:
             search_4_listfackel_bibl(listfackel_bibl)
         else:
+            # # If @corresp in listfackel.xml ends with "0u1", 
+            # # the @corresp should match an issue-element in 
+            # # fackelTexts_cascaded.xml - take that id instead.
             search_4_listfackel_issue_bibl(listfackel_bibl)
+    listfackel.tree_to_file(listfackel_path)
 
-print(f"{single_machtes} exact matches, {multimatches} unclear matches, {nomatches} couldn't be matched at all.")
-print("\n In {nolinkmatches} cases the link couldn't be matched.")
+print(f"{single_machtes} matches, {nomatches} couldn't be matched at all.")
