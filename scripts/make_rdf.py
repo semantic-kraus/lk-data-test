a2a8b1f1b1a2195a848df2da7920d3dc824c74ea
Daniel_Stoxreiter <elsner.daniel@outlook.com>
2023-06-29 14:07:44 +0200

    added model for place statements, identifiers, appellations: #56

--- scripts/make_rdf.py
+++ scripts/make_rdf.py
@@ -10,8 +10,8 @@ from acdh_cidoc_pyutils import (
 )
 from acdh_cidoc_pyutils.namespaces import CIDOC
 from acdh_tei_pyutils.tei import TeiReader
-from rdflib import Graph, Namespace, URIRef
-from rdflib.namespace import RDF, OWL
+from rdflib import Graph, Namespace, URIRef, Literal
+from rdflib.namespace import RDF, OWL, RDFS
 
 if os.environ.get("NO_LIMIT"):
     LIMIT = False
@@ -32,6 +32,8 @@ items = doc.any_xpath(f".//tei:{entity_type}")
 if LIMIT:
     items = items[:LIMIT]
 
+g.add((URIRef(f"{SK}types/place/placename"), RDF.type, CIDOC["E55_Type"]))
+g.add((URIRef(f"{SK}types/idno/xml-id"), RDF.type, CIDOC["E55_Type"]))
 
 print(f"converting {entity_type}s derived from {index_file}")
 for x in tqdm(items, total=len(items)):
@@ -76,6 +78,42 @@ for x in tqdm(items, total=len(items)):
             place_id_xpath="//tei:settlement[1]/@key",
         )
         g += birth_g
+    if x.xpath("./tei:birth[./tei:settlement]", namespaces=nsmap):
+        try:
+            birth_place_node = x.xpath(
+                "./tei:birth/tei:settlement", namespaces=nsmap
+            )[0]
+        except IndexError:
+            birth_place_node = None
+        if birth_place_node is not None:
+            try:
+                birth_place_id = birth_place_node.attrib["key"]
+            except KeyError:
+                birth_place_id = None
+            if birth_place_id is not None:
+                birth_place_uri = URIRef(f"{SK}{birth_place_id}")
+                g.add((birth_place_uri, RDF.type, CIDOC["E53_Place"]))
+                place_label = birth_place_node.xpath("./tei:placeName/text()", namespaces=nsmap)[0]
+                g.add((birth_place_uri, RDFS.label, Literal(place_label, lang="en")))
+                place_appellations = URIRef(f"{birth_place_uri}/appellations/0")
+                g.add((birth_place_uri, CIDOC["P1_is_identified_by"], place_appellations))
+                g.add((place_appellations, RDF.type, CIDOC["E33_E41_Linguistic_Appellation"]))
+                g.add((place_appellations, RDFS.label, Literal(place_label, lang="en")))
+                g.add((place_appellations, CIDOC["P2_has_type"], URIRef(f"{SK}types/place/placename")))
+                g.add((place_appellations, RDF.value, Literal(place_label)))
+                for i, idno in enumerate(birth_place_node.xpath("./tei:idno", namespaces=nsmap)):
+                    idno_uri = URIRef(f"{birth_place_uri}/identifiers/idno/{i}")
+                    g.add((birth_place_uri, CIDOC["P1_is_identified_by"], idno_uri))
+                    g.add((idno_uri, RDF.type, CIDOC["E42_Identifier"]))
+                    g.add((idno_uri, RDFS.label, Literal(f"Identifier: {idno.text}", lang="en")))
+                    g.add((idno_uri, CIDOC["P2_has_type"], URIRef(f"{SK}types/idno/URL/{idno.attrib['type']}")))
+                    g.add((idno_uri, RDF.value, Literal(idno.text, lang="en")))
+                birth_place_identifier_uri = URIRef(f"{birth_place_uri}/identifier/{birth_place_id}")
+                g.add((birth_place_uri, CIDOC["P1_is_identified_by"], birth_place_identifier_uri))
+                g.add((birth_place_identifier_uri, RDF.type, CIDOC["E42_Identifier"]))
+                g.add((birth_place_identifier_uri, RDFS.label, Literal(f"Identifier: {birth_place_id}", lang="und")))
+                g.add((birth_place_identifier_uri, CIDOC["P2_has_type"], URIRef(f"{SK}types/idno/xml-id")))
+                g.add((birth_place_identifier_uri, RDF.value, Literal(birth_place_id)))
     if x.xpath("./tei:death", namespaces=nsmap):
         death_g, death_uri, death_timestamp = make_birth_death_entities(
             subj,
@@ -88,6 +126,45 @@ for x in tqdm(items, total=len(items)):
             place_id_xpath="//tei:settlement[1]/@key",
         )
         g += death_g
+    if x.xpath("./tei:death[./tei:settlement]", namespaces=nsmap):
+        try:
+            death_place_node = x.xpath(
+                "./tei:death/tei:settlement", namespaces=nsmap
+            )[0]
+        except IndexError:
+            death_place_node = None
+        if death_place_node is not None:
+            try:
+                death_place_id = death_place_node.attrib["key"]
+            except KeyError:
+                death_place_id = None
+            if death_place_id is not None:
+                death_place_uri = URIRef(f"{SK}{death_place_id}")
+                g.add((death_place_uri, RDF.type, CIDOC["E53_Place"]))
+                place_label = death_place_node.xpath("./tei:placeName/text()", namespaces=nsmap)[0]
+                g.add((death_place_uri, RDFS.label, Literal(place_label, lang="en")))
+                place_appellations = URIRef(f"{death_place_uri}/appellations/0")
+                g.add((death_place_uri, CIDOC["P1_is_identified_by"], place_appellations))
+                g.add((place_appellations, RDF.type, CIDOC["E33_E41_Linguistic_Appellation"]))
+                g.add((place_appellations, RDFS.label, Literal(place_label, lang="en")))
+                g.add((place_appellations, CIDOC["P2_has_type"], URIRef(f"{SK}types/place/placename")))
+                g.add((place_appellations, RDF.value, Literal(place_label)))
+                for i, idno in enumerate(death_place_node.xpath("./tei:idno", namespaces=nsmap)):
+                    idno_uri = URIRef(f"{death_place_uri}/identifiers/idno/{i}")
+                    g.add((death_place_uri, CIDOC["P1_is_identified_by"], idno_uri))
+                    g.add((idno_uri, RDF.type, CIDOC["E42_Identifier"]))
+                    g.add((idno_uri, RDFS.label, Literal(f"Identifier: {idno.text}", lang="en")))
+                    g.add((idno_uri, CIDOC["P2_has_type"], URIRef(f"{SK}types/idno/URL/{idno.attrib['type']}")))
+                    g.add((idno_uri, RDF.value, Literal(idno.text, lang="en")))
+                death_place_identifier_uri = URIRef(f"{death_place_uri}/identifier/{death_place_id}")
+                g.add((death_place_uri,
+                       CIDOC["P1_is_identified_by"],
+                       death_place_identifier_uri))
+                g.add((death_place_uri, CIDOC["P1_is_identified_by"], death_place_identifier_uri))
+                g.add((death_place_identifier_uri, RDF.type, CIDOC["E42_Identifier"]))
+                g.add((death_place_identifier_uri, RDFS.label, Literal(f"Identifier: {death_place_id}", lang="und")))
+                g.add((death_place_identifier_uri, CIDOC["P2_has_type"], URIRef(f"{SK}types/idno/xml-id")))
+                g.add((death_place_identifier_uri, RDF.value, Literal(death_place_id)))
 
 # ORGS
 entity_type = "org"
