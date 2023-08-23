3f9aa8f0083a6325df4320795d10e40c415de64e
csae8092 <peter.andorfer@oeaw.ac.at>
2023-08-23 20:54:23 +0200

    ok, now all schnitzler-briefe should be corrected

--- scripts/fix_schnitzler_briefe.py
+++ scripts/fix_schnitzler_briefe.py
@@ -1,19 +1,22 @@
-from acdh_tei_pyutils.tei import TeiReader
+# don't run script more than once per entity type, not sure what will happen
 
-sb_url = "https://schnitzler-briefe.acdh.oeaw.ac.at/"
-entity_type = "person"
-index_file = f"./data/indices/list{entity_type}.xml"
-doc = TeiReader(index_file)
-nsmap = doc.nsmap
 
-for x in doc.any_xpath(".//tei:idno[@type='schnitzler-briefe']"):
-    pmb_id = x.text.split("=")[-1]
-    new_url = f"{sb_url}{pmb_id}.html"
-    x.text = new_url
+# from acdh_tei_pyutils.tei import TeiReader
 
-for x in doc.any_xpath(".//tei:idno[@subtype='schnitzler-briefe']"):
-    pmb_id = x.text.split("=")[-1]
-    new_url = f"{sb_url}{pmb_id}.html"
-    x.text = new_url
+# sb_url = "https://schnitzler-briefe.acdh.oeaw.ac.at/"
+# entity_type = "work"
+# index_file = f"./data/indices/list{entity_type}.xml"
+# doc = TeiReader(index_file)
+# nsmap = doc.nsmap
 
-doc.tree_to_file(index_file)
+# for x in doc.any_xpath(".//tei:idno[@type='schnitzler-briefe']"):
+#     pmb_id = x.text.split("=")[-1]
+#     new_url = f"{sb_url}{pmb_id}.html"
+#     x.text = new_url
+
+# for x in doc.any_xpath(".//tei:idno[@subtype='schnitzler-briefe']"):
+#     pmb_id = x.text.split("=")[-1]
+#     new_url = f"{sb_url}{pmb_id}.html"
+#     x.text = new_url
+
+# doc.tree_to_file(index_file)
