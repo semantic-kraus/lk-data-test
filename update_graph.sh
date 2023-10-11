69537f287a759b85d7d722cfcef193d747c08bc7
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-10-11 15:09:48 +0200

    minor udpate

--- update_graph.sh
+++ update_graph.sh
@@ -5,19 +5,19 @@ curl -D- -X DELETE \
     'https://sk-blazegraph.acdh-dev.oeaw.ac.at/blazegraph/sparql?c=<https://sk.acdh.oeaw.ac.at/project/legal-kraus>&c=<https://sk.acdh.oeaw.ac.at/provenance>&c=<https://sk.acdh.oeaw.ac.at/model>&c=<https://sk.acdh.oeaw.ac.at/general>'
 
 curl -u $R_USER \
-    'https://sk-blazegraph.acdh-dev.oeaw.ac.at/blazegraph/sparql' \
+    $R_ENDPOINT \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
     -H 'Accept: text/boolean' \
     -d @rdf/data.trig
 
 curl -u $R_USER \
-    'https://sk-blazegraph.acdh-dev.oeaw.ac.at/blazegraph/sparql' \
+    $R_ENDPOINT \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
     -H 'Accept: text/boolean' \
     -d @rdf/texts.trig
 
 curl -u $R_USER \
-    'https://sk-blazegraph.acdh-dev.oeaw.ac.at/blazegraph/sparql' \
+    $R_ENDPOINT \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
     -H 'Accept: text/boolean' \
     -d @rdf/work.trig
