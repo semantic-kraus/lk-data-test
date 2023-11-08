c9a5ac3dabb39cbc959f4e1de98c6f5bd7e5900a
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-11-08 10:59:30 +0100

    added sleep intervals

--- update_graph.sh
+++ update_graph.sh
@@ -1,21 +1,28 @@
 # bin/bash
 
+echo "delete namedgraphs"
 curl -D- -X DELETE \
     -u $R_USER \
     'https://sk-blazegraph.acdh-dev.oeaw.ac.at/blazegraph/sparql?c=<https://sk.acdh.oeaw.ac.at/project/legal-kraus>&c=<https://sk.acdh.oeaw.ac.at/provenance>&c=<https://sk.acdh.oeaw.ac.at/model>&c=<https://sk.acdh.oeaw.ac.at/general>'
+sleep 60
 
+echo "add namedgraph data.trig"
 curl -u $R_USER \
     $R_ENDPOINT \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
     -H 'Accept: text/boolean' \
     -d @rdf/data.trig
+sleep 60
 
+echo "add namedgraph texts.trig"
 curl -u $R_USER \
     $R_ENDPOINT \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
     -H 'Accept: text/boolean' \
     -d @rdf/texts.trig
+sleep 60
 
+echo "add namedgraph work.trig"
 curl -u $R_USER \
     $R_ENDPOINT \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
