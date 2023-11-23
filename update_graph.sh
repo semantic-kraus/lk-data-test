bec0cfd30bbbb4657a9ce3100cbac20a69d7ee58
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-11-23 11:08:27 +0100

    update curl url

--- update_graph.sh
+++ update_graph.sh
@@ -2,29 +2,25 @@
 
 echo "delete namedgraphs"
 curl -D- -X DELETE \
-    -u $R_USER_V \
-    'https://varuna.arz.oeaw.ac.at:8080/sk/sparql?c=<https://sk.acdh.oeaw.ac.at/project/legal-kraus>&c=<https://sk.acdh.oeaw.ac.at/provenance>&c=<https://sk.acdh.oeaw.ac.at/model>&c=<https://sk.acdh.oeaw.ac.at/general>'
+    "{$R_ENDPOINT_V}?c=<https://sk.acdh.oeaw.ac.at/project/legal-kraus>&c=<https://sk.acdh.oeaw.ac.at/provenance>&c=<https://sk.acdh.oeaw.ac.at/model>&c=<https://sk.acdh.oeaw.ac.at/general>"
 sleep 300
 
 echo "add namedgraph data.trig"
-curl -u $R_USER_V \
-    $R_ENDPOINT_V \
+curl $R_ENDPOINT_V \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
     -H 'Accept: text/boolean' \
     -d @rdf/data.trig
 sleep 600
 
 echo "add namedgraph texts.trig"
-curl -u $R_USER_V \
-    $R_ENDPOINT_V \
+curl $R_ENDPOINT_V \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
     -H 'Accept: text/boolean' \
     -d @rdf/texts.trig
 sleep 600
 
 echo "add namedgraph work.trig"
-curl -u $R_USER_V \
-    $R_ENDPOINT_V \
+curl $R_ENDPOINT_V \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
     -H 'Accept: text/boolean' \
     -d @rdf/work.trig
